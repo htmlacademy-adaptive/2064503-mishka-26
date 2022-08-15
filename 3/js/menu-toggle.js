2dded4e9dd00da825feb4bb1bf85facbfe6792f0
Keks <keks@htmlacademy.ru>
2022-08-15 16:37:56 +0300

    :heavy_check_mark: Сборка #3

--- 3/js/menu-toggle.js
+++ 3/js/menu-toggle.js
@@ -1,15 +1,15 @@
-var modalMenu = document.getElementsByClassName('main-nav__modal-window')[0];
-var buttonOpen = document.getElementById('toggle-menu-open');
-var buttonClose = document.getElementById('toggle-menu-close');
+let modalMenu = document.getElementsByClassName('main-nav__modal-window')[0];
+let buttonOpen = document.getElementById('toggle-menu-open');
+let buttonClose = document.getElementById('toggle-menu-close');
 
-buttonOpen.onclick = function () {
+buttonOpen.addEventListener('click', function() {
   buttonOpen.classList.add("toggle-close");
   buttonClose.classList.remove("toggle-close");
   modalMenu.classList.toggle("main-nav__modal-window-open");
-}
+})
 
-buttonClose.onclick = function () {
+buttonClose.addEventListener('click', function() {
   buttonClose.classList.add("toggle-close");
   buttonOpen.classList.remove("toggle-close");
   modalMenu.classList.toggle("main-nav__modal-window-open");
-}
+})
