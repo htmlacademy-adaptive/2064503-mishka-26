86e154ee2ed6d04ca52e893055ee70aa413be267
Grony <gr0ny.0017@gmail.com>
2022-08-23 18:36:22 +0300

    Блок с видео

--- source/js/menu-toggle.js
+++ source/js/menu-toggle.js
@@ -3,13 +3,13 @@ let buttonOpen = document.getElementById('toggle-menu-open');
 let buttonClose = document.getElementById('toggle-menu-close');
 
 buttonOpen.addEventListener('click', function() {
-  buttonOpen.classList.add("toggle-close");
-  buttonClose.classList.remove("toggle-close");
+  buttonOpen.classList.add("toggle--close");
+  buttonClose.classList.remove("toggle--close");
   modalMenu.classList.toggle("main-nav__modal-window-open");
 })
 
 buttonClose.addEventListener('click', function() {
-  buttonClose.classList.add("toggle-close");
-  buttonOpen.classList.remove("toggle-close");
+  buttonClose.classList.add("toggle--close");
+  buttonOpen.classList.remove("toggle--close");
   modalMenu.classList.toggle("main-nav__modal-window-open");
 })
